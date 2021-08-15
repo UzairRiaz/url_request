137bf49f3384bb7773ef477463ebcc94ae83900a
uzairriaz <uzairriaz.pk@gmail.com>
2021-08-15 08:47:32 +0500

    Initial Commit

---
+++ Twitter_clone_Firebase/Twitter_clone_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_clone_FirebaseApp.swift
+//  Twitter_clone_Firebase
+//
+//  Created by Umair Riaz on 15/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_clone_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
